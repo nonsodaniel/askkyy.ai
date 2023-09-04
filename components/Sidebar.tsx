053c99f4554c1f493141f82270f4f89fa37b1e2d
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-04 17:00:30 +0100

    Add active state to sidebar navs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import { Montserrat, Poppins } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const monsterat = Poppins({
   weight: "600",
@@ -20,6 +21,10 @@ const monsterat = Poppins({
 });
 
 const Sidebar = () => {
+  const pathName = usePathname();
+  const activePathColor = (href: string) =>
+    href === pathName ? "text-white bg-white/10" : "text-zinc-100";
+
   const routes = [
     {
       label: "Dashboard",
@@ -81,7 +86,11 @@ const Sidebar = () => {
             <Link
               key={route.href}
               href={route.href}
-              className="flex justify-start  font-medium p-3 w-full cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                `flex justify-start  font-medium p-3 w-full cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition 
+              `,
+                activePathColor(route.href)
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
