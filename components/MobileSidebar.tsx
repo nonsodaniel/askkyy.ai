5976f5e45e6fddacb1cd4adca332d05e1b5fe37e
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-04 16:41:25 +0100

    Fix hrydration bug

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,11 +1,17 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./Sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); //to avoid Hydration
+
+  if (!isMounted) return null;
   return (
     <Sheet>
       <SheetTrigger>
