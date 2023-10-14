685ba21216a983e413bec7fb98a2bc78cfe6c41f
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-14 19:26:28 +0100

    Implemented code generation

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,139 @@
+"use client";
+import Header from "@/components/Header";
+import { formSchema } from "@/utils/constants";
+import { Code } from "lucide-react";
+import React, { useState } from "react";
+import { Form, useForm, FormProvider } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
+import Loading from "@/components/ui/Loading";
+import { Empty } from "@/components/ui/Empty";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/ui/UserAvatar";
+import { BotAvatar } from "@/components/ui/BotAvatar";
+import ReactMarkDown from "react-markdown";
+
+const CodePage = () => {
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+  const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const latestMessages = [...messages, userMessage];
+      const response = await axios.post("/api/code", {
+        messages: latestMessages,
+      });
+      setMessages((current) => [...current, userMessage, response.data]);
+      form.reset();
+    } catch (error) {
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"Code Generation Section"}
+        description={"Welcome to your AI powered Conversation"}
+        icon={Code}
+        iconColor="text-blue-500"
+        bgColor="bg-blue-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div className="">
+          <FormProvider {...form}>
+            <Form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="rounded-lg  border w-full  p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Which code do you want to generate?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
+                disabled={isLoading}
+                size="icon"
+              >
+                Generate
+              </Button>
+            </Form>
+          </FormProvider>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loading />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started." />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                  message.role === "user"
+                    ? "bg-white border border-black/10"
+                    : "bg-muted"
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                <ReactMarkDown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || ""}
+                </ReactMarkDown>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
