397c04447ada13bdbdba6557a236d77917a1b791
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-14 23:35:04 +0100

    restyled logo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -29,12 +29,13 @@ const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#625df5b3] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
-          <div className="relative w-8 h-8 mr-4">
-            <Image alt="logo" src="/logo.png" fill />
-          </div>
-          <h1 className={cn(`text-2xl font-bold`, monsterat.className)}>
-            Askkyy
+        <Link
+          href={"/dashboard"}
+          className="flex items-center justify-center pl-3 mb-14"
+        >
+          <h1 className={cn(`font-bold text-center`, monsterat.className)}>
+            <span className="text-4xl text-orange-400">Askkyy</span>
+            <span>.ai</span>
           </h1>
         </Link>
         <div className="space-y-1">
