097793b2c89f5596d29e559be942912531664a7f
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-15 15:18:33 +0100

    Configured Prisma Api Limit settings

--- lib/prismaDb.ts
+++ lib/prismaDb.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prismaDb = globalThis || new PrismaClient();
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient | undefined;
+    }
+  }
+}
+
+const prismaDb = globalThis.prisma || new PrismaClient();
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismaDb;
 
 export default prismaDb;
