c75347d16b14f93f1af1acc9e525ad636a3849ac
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-31 16:02:58 +0100

    Implemented DB creation for Music

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
+import { saveResponseToFirebase } from "@/utils/helpers";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
@@ -33,7 +34,7 @@ export async function POST(req: Request) {
       );
     }
 
-    const response = await replicate.run(
+    const response: any = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
         input: {
@@ -41,13 +42,27 @@ export async function POST(req: Request) {
         },
       }
     );
-    await incrementApiLimit();
 
-    if (!isPro) {
+    if (response) {
+      const requestData = {
+        question: prompt,
+        answer: response.audio,
+        createdBy: userId,
+        id: Date.now(),
+      };
+
+      saveResponseToFirebase(requestData, "Music");
+
       await incrementApiLimit();
+
+      if (!isPro) {
+        await incrementApiLimit();
+      }
+
+      return NextResponse.json(response);
     }
 
-    return NextResponse.json(response);
+    await incrementApiLimit();
   } catch (error) {
     console.log("[MUSIC_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
